4e7098e3ff236290ccf420126c26bd56c65c72da
TrueXRetro <dragonminner867@outlook.com>
2019-02-13 20:31:54 +0100

    Small changes

--- commands/purge.js
+++ commands/purge.js
@@ -6,9 +6,8 @@ exports.run = async (client, message, args) => {
     if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('You must specify the number of text to delete');
     if(!args[0]) return message.channel.send('You must specify the number of messages to delete');
 
-    // Sending to chat
-    message.channel.bulkDelete(args[0]).then(() => { // This deletes the command from the user
+    message.channel.bulkDelete(args[0]).then(() => { 
         message.channel.send(`:pencil2: ${args[0]} messages have been deleted.`).then(msg => msg.delete(5000));
     });
 
-} // Now, we can test it!
+} 
