96119cebbc73c77b443a1d848b5836f147e7c8ea
TrueXRetro <dragonminner867@outlook.com>
2019-05-02 13:34:52 +0200

    Update reload.js

--- commands/reload.js
+++ commands/reload.js
@@ -1,8 +1,4 @@
 exports.run = (client, message, args, ops) => {
-
-    // We're going to be passing an extra argument, titled 'ops'
-    // We can choose what to pass into it via the index.js
-    // Now, we can access ops.ownerID & it will return the ID defined at the top of index.js
     
     if (message.author.id !== ops.ownerID) return message.channel.send('Sorry, only the bot owner can use this command.');
 
