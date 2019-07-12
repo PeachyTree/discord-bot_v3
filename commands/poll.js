17e711bb4550557ae8c1728c7b2a9923fa6f6deb
mlp_Pinkie_Pie <dragonminner867@outlook.com>
2019-07-12 08:34:58 +0200

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -1,5 +1,4 @@
-// This command requires the discord-js package to create embeds
-const Discord = require('discord.js');
+const {RichEmbed} = require('discord.js');
 
 exports.run = async (client, message, args, tools) => {
 
@@ -11,17 +10,16 @@ exports.run = async (client, message, args, tools) => {
 
     if (!args[0]) return message.channel.send('Proper Usage: <prefix>poll question');
 
-    const embed = new Discord.RichEmbed()
+    const embed = new RichEmbed()
         .setColor(0xffffff)
         .setFooter('React to vote.')
         .setDescription(args.join(' '))
         .setTitle(`Poll created by ${message.author.username}`);
-
-    let msg = await message.channel.send(embed);
+    let msg = await message.channel.send({embed});
 
     await msg.react('✅'); 
     await msg.react('❌');
 
-    message.delete({timeout: 1000}); // This waits 1000 milliseconds before deleting (1 second)
+    message.delete({timeout: 1000}); // This waits 1000 milliseconds before deleting (1 second). You can change it.
 
 } 
