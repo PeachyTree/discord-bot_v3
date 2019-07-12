3ebe9a03905e8a41cdbf9271337744a2c853de09
mlp_Pinkie_Pie <dragonminner867@outlook.com>
2019-07-12 08:34:02 +0200

    Small fixes

--- commands/ping.js
+++ commands/ping.js
@@ -1,23 +1,17 @@
-// This commands requires 1 package
-const Discord = require('discord.js'); // We need this to form & send embeds.
+const {RichEmbed} = require('discord.js'); 
 
-// Command Handler
 exports.run = async (client, message, args) => {
 
-    const useruser = 'Command ran by: ' + message.author.username;
-    const userurl = message.author.avavtarURL;
-
-    // Forming the embed
-    let embed = new Discord.RichEmbed() // Remember to use .MessageEmbed() if you use the master version
+    let embed = new RichEmbed() 
         .setColor(0x43f033)
         .setDescription(`Loading...`)
         .setTimestamp()
-    message.channel.send(embed).then(message => {
+    message.channel.send{(embed}).then(message => {
         embed.setColor(0x43f033)
         embed.setDescription(`:ping_pong: Pong! **\`${client.pings[0]}ms\`**`)
-        embed.setFooter(useruser, userurl)
+        embed.setFooter(`Command ran by ${message.author.username}`, message.author.avavtarURL)
         embed.setTimestamp()
-        message.edit(embed)
+        message.edit({embed})
     })
 
 } 
