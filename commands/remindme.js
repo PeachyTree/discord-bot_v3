659d9e81ed75a5eb224bd46c87a7e273c8151d24
mlp_Pinkie_Pie <dragonminner867@outlook.com>
2019-07-12 08:36:56 +0200

    Fixed embeds

--- commands/remindme.js
+++ commands/remindme.js
@@ -1,38 +1,33 @@
-// We require 2 packages for this command, discord.js & ms
-const Discord = require('discord.js');
+const {RichEmbed} = require('discord.js');
 const ms = require('ms');
 
-// Command Handler - You can use your own
 exports.run = async (client, message, args) => {
 
     let reminderTime = args[0]; 
     if (!reminderTime) {
-        let embed = new Discord.RichEmbed() 
+        let embed = new RichEmbed() 
             .setTitle('Proper Usage') 
             .setDescription(`\`<prefix>remindme 15min any text or code\``)
-
-        return message.channel.send(embed) 
+        return message.channel.send({embed}) 
     }
 
     let reminder = args.slice(1).join(" "); 
 
-    let remindEmbed = new Discord.RichEmbed() 
+    let embed = new RichEmbed() 
         .setColor('#ffffff')
         .setAuthor(`${message.author.username}`, message.author.displayAvatarURL)
         .addField('Reminder', `\`\`\`${reminder}\`\`\``) 
         .addField('Time', `\`\`\`${reminderTime}\`\`\``) 
         .setTimestamp();
-
-    message.channel.send(remindEmbed); 
+    message.channel.send({embed}); 
 
     setTimeout(function() {
-        let remindEmbed = new Discord.RichEmbed()
+        let embed = new RichEmbed()
             .setColor('#ffffff')
             .setAuthor(`${message.author.username}`, message.author.displayAvatarURL)
             .addField('Reminder', `\`\`\`${reminder}\`\`\``)
             .setTimestamp()
-
-            message.channel.send(remindEmbed);
+            message.channel.send({embed});
     }, ms(reminderTime));
 
 } 
