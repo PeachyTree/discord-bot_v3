93c9c080eb4ea8025b89d01c63cd1ee7d42d440f
Azura Apple <dragonminner867@outlook.com>
2020-04-16 20:14:34 +0200

    added license

--- commands/reload.js
+++ commands/reload.js
@@ -1,3 +1,5 @@
+// Copyright (c) 2020 Azura Apple. All rights reserved. MIT license.
+
 exports.run = (client, message, args, ops) => {
     
     if (message.author.id !== ops.ownerID) return message.channel.send('Sorry, only the bot owner can use this command.');
